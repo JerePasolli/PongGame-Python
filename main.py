d89fd015aaa1fa28b576d61b00f4aebf39d0c371
JerePasolli <jeremiaspasolli@gmail.com>
2023-11-27 21:31:37 -0300

    Pong game with Python

---
+++ main.py
@@ -0,0 +1,52 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+scoreboard.update_scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # detect collision with upper and lower walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect missing ball right side
+    if ball.xcor() > 390:
+        ball.reset()
+        scoreboard.l_point()
+        scoreboard.update_scoreboard()
+
+    # detect missing ball left side
+    if ball.xcor() < -390:
+        ball.reset()
+        scoreboard.r_point()
+        scoreboard.update_scoreboard()
+
+
+screen.exitonclick()
